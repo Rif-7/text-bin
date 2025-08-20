d91ec1b6ae139a5ef866e832c684fd5c25722263
Rifan <muhammadrifan974530@gmail.com>
2025-08-20 20:07:19 +0530

    add home, create and view routes

--- main.go
+++ main.go
@@ -1,19 +1,46 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Server", "Go")
 	w.Write([]byte("Hello, world"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a snippet create form"))
+}
+
+func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Save a new snippet"))
 }
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
 
-	log.Print("starting server on :4000")
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+
+	log.Print("starting server on http://127.0.0.1:4000")
 
 	err := http.ListenAndServe("127.0.0.1:4000", mux)
 
