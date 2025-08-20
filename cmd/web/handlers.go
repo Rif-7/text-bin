7643fdccf7372c6732e307e7fbb4d7fbb2c4ef4a
Rifan <muhammadrifan974530@gmail.com>
2025-08-20 21:44:13 +0530

    add templates and static files

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,13 +2,35 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
-	w.Write([]byte("Hello from TextBin"))
+
+	files := []string{
+		"./ui/html/partials/nav.html",
+		"./ui/html/base.html",
+		"./ui/html/pages/home.html",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
 }
 func snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.PathValue("id"))
